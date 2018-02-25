6b1ec6a9a09cc0e370839e82334c7adfe8ab6976
Lily Yang <zq.yang.lily@gmail.com>
2018-02-24 23:05:50 -0800

    created app

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  context: __dirname,
+  entry: 'entry.jsx',
+  output: {
+    filename: './bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
