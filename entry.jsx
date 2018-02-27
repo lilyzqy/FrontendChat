c57fb1c1c8599f59be5e7fe79237e28d38206480
Lily Yang <zq.yang.lily@gmail.com>
2018-02-27 11:43:51 -0800

    get entry file started, created reducers and root component

--- entry.jsx
+++ entry.jsx
@@ -1,2 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import RootReducer from './reducers/root_reducer';
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded',()=>{
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={createStore(RootReducer)}/>);
+});
