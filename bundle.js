2d5f9b85e7e60bc13ed5aedcde9a8d39b0d8e678
Lily Yang <zq.yang.lily@gmail.com>
2018-02-28 13:01:28 -0800

    got message list in chat view

--- bundle.js
+++ bundle.js
@@ -21505,6 +21505,7 @@ var ChatView = function (_React$Component) {
             )
           )
         ),
+        _react2.default.createElement(_message_list2.default, null),
         _react2.default.createElement(_input_bar2.default, null)
       );
     }
@@ -21572,6 +21573,45 @@ exports.default = InputBar;
 
 "use strict";
 
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(2);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var MessageList = function (_React$Component) {
+  _inherits(MessageList, _React$Component);
+
+  function MessageList() {
+    _classCallCheck(this, MessageList);
+
+    return _possibleConstructorReturn(this, (MessageList.__proto__ || Object.getPrototypeOf(MessageList)).apply(this, arguments));
+  }
+
+  _createClass(MessageList, [{
+    key: "render",
+    value: function render() {
+      return _react2.default.createElement("section", { className: "message-list" });
+    }
+  }]);
+
+  return MessageList;
+}(_react2.default.Component);
+
+exports.default = MessageList;
 
 /***/ }),
 /* 81 */
---
+++ components/messages/message_list.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MessageList extends React.Component{
+  render(){
+    return (
+      <section className="message-list">
+
+      </section>
+    );
+  }
+}
+
+export default MessageList;
